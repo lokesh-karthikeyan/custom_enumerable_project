e75aee92a03e1b27615d8027ee38595868655136
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-11-24 23:44:36 +0530

    Refactor `Array#my_each` | Add `Hash#my_each`
     + Removed the "each()" method and implemented a different iterator -> "times()" method.
     + It iterates through each element inside the block & returns the original block.
     + Same "my_each()" also implemented in "Hash" class.
     + Get the keys in array format, with the index of the keys array found the value.
    
    These changes made the test pass & also the right way for implementing custom enumerables.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
+# This is an extension for the module 'Enumerable'
 module Enumerable
   # Your code goes here
+  def my_each_with_index(&block); end
 end
 
 # You will first have to define my_each
@@ -8,7 +12,16 @@ end
 # to this method
 class Array
   # Define my_each here
-  def my_each(&block)
-    each(&block)
+  def my_each
+    length.times { |index| yield(self[index]) }
+    self
+  end
+end
+
+# Custom methods for 'Hash' class
+class Hash
+  def my_each
+    keys.length.times { |index| yield(keys[index], self[keys[index]]) }
+    self
   end
 end
