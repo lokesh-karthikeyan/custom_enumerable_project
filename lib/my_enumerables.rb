74e0b6e1d8f1ae10bd19999dffc85f7c2dd8c1db
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-11-25 15:24:21 +0530

    Add `Enumerable#my_select` method
     + Creates an empty "[]" or "{}" based on the called object's class.
     + Added logic to select only the element from the block that yields true.
     + The items return true will be added to the empty data structure, the other items are just ignored.
     + Finally, the method returns the "filtered_items".
    
    These changes made the test pass successfully.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -12,6 +12,19 @@ module Enumerable
     0.upto(keys.length - 1) { |index| yield(keys[index], self[keys[index]], index) }
     self
   end
+
+  # rubocop:disable Metrics/AbcSize
+  def my_select
+    filtered_items = self.class.new
+    length.times do |index|
+      filtered_items << self[index] if filtered_items.is_a?(Array) && yield(self[index])
+      if filtered_items.is_a?(Hash) && yield(keys[index], self[keys[index]])
+        filtered_items[keys[index]] = self[keys[index]]
+      end
+    end
+    filtered_items
+  end
+  # rubocop:enable Metrics/AbcSize
 end
 
 # You will first have to define my_each
